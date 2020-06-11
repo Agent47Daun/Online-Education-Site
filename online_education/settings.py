e17c908360c5d6097bc5fe8911f7984c9cc516ed
AgentDaun <agent47daun@gmail.com>
2020-06-11 16:20:47 +0400

    Added rest-framework, knox and djoser to project

--- online_education/settings.py
+++ online_education/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'knox',
+    'djoser',
+
+    'user',
 ]
 
 MIDDLEWARE = [
@@ -122,6 +127,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Rest Framework
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': ('knox.auth.TokenAuthentication',),
+}
 
 
 # Celery
