30ab6865bc5a008c60d9d79e200a4520a1c25066
AgentDaun <agent47daun@gmail.com>
2020-06-15 14:13:35 +0400

    RetrieveUpdateUser test + UserLogin test updated

--- user/tests.py
+++ user/tests.py
@@ -4,11 +4,11 @@ from rest_framework.test import APITestCase
 
 from .models import User
 
+
 class UserRegistrationTest(APITestCase):
     url = reverse("user-list")
 
     def test_valid_data(self):
-
         user_data = {
             "username": "test_username",
             "password": "test_password",
@@ -19,7 +19,6 @@ class UserRegistrationTest(APITestCase):
         self.assertEqual(201, response.status_code)
 
     def test_unique_username_email(self):
-
         user_data = {
             "username": "test_username",
             "password": "test_password",
@@ -49,6 +48,7 @@ class UserRegistrationTest(APITestCase):
 
 class UserLoginTest(APITestCase):
     url = reverse("knox_login")
+    token_test_url = reverse('user-me')
 
     def setUp(self):
         self.username = "test_username"
@@ -67,3 +67,30 @@ class UserLoginTest(APITestCase):
     def test_valid_data(self):
         response = self.client.post(self.url, {"username": self.username, "password": self.password})
         self.assertEqual(200, response.status_code)
+        self.assertTrue(response.data.get("token"))
+
+        #  Token test
+        token = response.data['token']
+        self.client.credentials(HTTP_AUTHORIZATION="Token " + token)
+        test_token_response = self.client.get(self.token_test_url)
+        self.assertEqual(200, test_token_response.status_code)
+
+
+class UserRetrieveUpdateTest(APITestCase):
+    url = reverse("user-me")
+
+    def setUp(self):
+        self.username = "test_username"
+        self.password = "test_password"
+        self.email = "test_user@test.com"
+        self.user = User.objects.create_user(username=self.username, password=self.password, email=self.email)
+
+    def test_unauthorized_retrieve(self):
+        response = self.client.get(self.url)
+        self.assertEqual(401, response.status_code)
+
+    def test_authorized_retrieve(self):
+        self.client.force_authenticate(self.user)
+        response = self.client.get(self.url)
+        self.assertEqual(200, response.status_code)
+        self.assertEqual(self.username, response.data['username'])
