63ba0339b1ad31e8de61f4b64b30ac5a77aee782
AgentDaun <agent47daun@gmail.com>
2020-07-23 19:51:02 +0400

    removed djoser url from user app

--- user/urls.py
+++ user/urls.py
@@ -7,5 +7,4 @@ urlpatterns = [
     path('login/', LoginView.as_view(), name='knox_login'),
     path('logout/', knox_views.LogoutView.as_view(), name='knox_logout'),
     path('logoutall/', knox_views.LogoutAllView.as_view(), name='knox_logoutall'),
-    path('auth/', include('djoser.urls')),
 ]
