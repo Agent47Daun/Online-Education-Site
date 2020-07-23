4df5c4321ba6eb1ef8de8d738dcce27797f30481
AgentDaun <agent47daun@gmail.com>
2020-07-23 20:09:38 +0400

    added student and teacher accounts

--- user/models.py
+++ user/models.py
@@ -1,6 +1,19 @@
 from django.db import models
+from django.conf import settings
 from django.contrib.auth.models import AbstractUser
 # Create your models here.
+
+
+class StudentAccount(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL,
+                                on_delete=models.CASCADE,
+                                related_name='student_account')
+
+
+class TeacherAccount(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL,
+                                on_delete=models.CASCADE,
+                                related_name='teacher_account')
 
 
 class User(AbstractUser):
