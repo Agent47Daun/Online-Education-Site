13907bae413adb73d2f16a47e79a636218935277
AgentDaun <agent47daun@gmail.com>
2020-07-23 20:15:56 +0400

    added model Classroom

--- classroom/models.py
+++ classroom/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from user.models import TeacherAccount, StudentAccount
 
 # Create your models here.
+
+
+class Classroom(models.Model):
+    teacher = models.ForeignKey(TeacherAccount,
+                                on_delete=models.CASCADE,
+                                related_name="classrooms")
+    students = models.ManyToManyField(StudentAccount,
+                                      related_name="classrooms")
+    
