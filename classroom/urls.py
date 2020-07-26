05b277452e8f1914463b4d04deb8900bc3039e23
AgentDaun <agent47daun@gmail.com>
2020-07-26 17:27:16 +0400

    typo fixed

--- classroom/urls.py
+++ classroom/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path, include
 
-from .views import ClassroomCreateListApiView, ClassroomAddLesson, ClassroomRetrieveUpdateDestroyApiView, RetrieveUpdateDestroyApiView
+from .views import ClassroomCreateListApiView, ClassroomAddLesson, ClassroomRetrieveUpdateDestroyApiView, LessonRetrieveUpdateDestroyApiView
 urlpatterns = [
     path('', ClassroomCreateListApiView.as_view(), name="classroom_list_create"),
     path('<int:pk>/', ClassroomRetrieveUpdateDestroyApiView.as_view(), name="classroom_retrieve_update_destroy"),
     path('<int:pk>/add_lesson/', ClassroomAddLesson.as_view(), name="classroom_add_lesson"),
-    path("lesson/<int:pk>/", RetrieveUpdateDestroyApiView.as_view(), name="classroom_lesson_retrieve_update_destroy")
+    path("lesson/<int:pk>/", LessonRetrieveUpdateDestroyApiView.as_view(), name="classroom_lesson_retrieve_update_destroy")
 ]
