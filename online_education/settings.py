5ec5f6c2de680942989422d2397d23b4bfae39a8
AgentDaun <agent47daun@gmail.com>
2020-07-26 17:42:44 +0400

    added SessionAutnhetication only for testing

--- online_education/settings.py
+++ online_education/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import sys
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -127,11 +128,21 @@ AUTH_USER_MODEL = "user.User"
 
 # Rest Framework
 REST_FRAMEWORK = {
-    'DEFAULT_AUTHENTICATION_CLASSES': ('knox.auth.TokenAuthentication', 'rest_framework.authentication.SessionAuthentication'),
+    'DEFAULT_AUTHENTICATION_CLASSES': ['knox.auth.TokenAuthentication', ],
     'DEFAULT_PERMISSION_CLASSES': (
         'rest_framework.permissions.IsAuthenticated',
     ),
 }
+
+#  SessionAuthentication for force_login in test.py
+if 'test' in sys.argv:
+    REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': ['knox.auth.TokenAuthentication', "rest_framework.authentication.SessionAuthentication"],
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    ),
+}
+
 
 # Knox
 REST_KNOX = {
